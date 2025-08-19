349e67b0be5aab88524c0a8da313db96ba597c12
Shani-Kumar-Gupta <shani.gupta_cse@outlook.com>
2025-08-19 22:16:02 +0530

    Sending Response to client with some header (Meta data)

--- Projects/my-node-ts-app/src/index.ts
+++ Projects/my-node-ts-app/src/index.ts
@@ -2,8 +2,16 @@ import * as http from 'node:http';
 
 const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
   console.log("Request Body : ", req);
-  res.end('Hello World');
+  // res.end('Hello World');
   // process.exit(); /// To exit the process
+  res.setHeader('Content-Type', 'text/html');
+  res.write(`
+    <html>
+    <head><title>My Web</title></head>
+    <body>Hello From <b>Nodejs</b> Server</body>
+    </html>
+    `);
+  res.end();
 });
 
 server.listen(3000, 'localhost', () => {
